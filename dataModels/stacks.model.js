b472c92b7d5fc943edd5e08bc9ea54ad2fbb9b50
JordyV23 <jordyvargas0123@gmail.com>
2023-02-20 09:21:25 -0600

    Mejoras en las consultas

--- dataModels/stacks.model.js
+++ dataModels/stacks.model.js
@@ -1,16 +1,11 @@
 const mariadb = require('mariadb');
 const mariaConfig = require('../config/maria.Config')
-const {
-    response
-} = require("express");
-const {
-    consulta
-} = require('../generator/ConsultasSQL')
+const { response } = require("express");
+const { consulta } = require('../generator/ConsultasSQL')
 
 const getStacks = async () => {
     return consulta(mariadb, mariaConfig, 'SELECT * FROM tbfrontend')
 }
-
 
 const postStacks = async (req, res = response) => {
     try {
@@ -24,9 +19,7 @@ const postStacks = async (req, res = response) => {
 const deleteStacks = async (req, res = response) => {
     try {
         const { id } = req.body
-        let pool = mariadb.createPool(mariaConfig);
-        await pool.query('DELETE FROM tbfrontend WHERE Id=?', id);
-        res.send("Eliminado con exito")
+        consulta(mariadb, mariaConfig, `DELETE FROM tbfrontend WHERE Id='${id}'`).then(res.send("Eliminado con exito"))
     } catch (error) {
         console.log(error);
     }
@@ -34,13 +27,8 @@ const deleteStacks = async (req, res = response) => {
 
 const updateStacks = async (req, res = response) => {
     try {
-        const {
-            nombre,
-            id
-        } = req.body
-        let pool = mariadb.createPool(mariaConfig);
-        await pool.query(`UPDATE tbfrontend SET Nombre='${nombre}' WHERE Id=${id}`);
-        res.send("Actualizado con exito")
+        const {nombre,id} = req.body;
+        consulta(mariadb, mariaConfig, `UPDATE tbfrontend SET Nombre='${nombre}' WHERE Id=${id}`).then(res.send("Actualizado con exito"))
     } catch (error) {
         console.log(error);
     }
